7f43f058301f0ae9838d1b2223f4e6331a502080
TlStephen2011 <tlstephen11@gmail.com>
2017-05-18 17:53:02 +0200

    day_25

---
+++ C++/Day25_TimeComplexity/main.cpp
@@ -0,0 +1,49 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    vector<int> data;
+    for (int i = 0; i < T; i++)
+    {
+        int n;
+        cin >> n;
+        data.push_back(n);
+    }
+
+    for (int i = 0; i < data.size(); i++)
+    {
+        bool isPrime = true;
+        if (data[i] != 1)
+        {
+            for (int j = 2; j < data[i]; j++)
+            {
+                if (data[i] % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            isPrime = false;
+        }
+
+        if (isPrime)
+        {
+            cout << "Prime" << endl;
+        }
+        else
+        {
+            cout << "Not prime" << endl;
+        }
+    }
+    return 0;
+}
